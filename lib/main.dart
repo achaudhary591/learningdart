93115b5cbee8b113096f04044688086307303b2c
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-12-27 07:19:41 +0530

    fucntion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+String getFullName(String firstName, String secondName) {
+  return firstName + " " + secondName;
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,6 +14,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print(getFullName("akshay", "chaudhary"));
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
